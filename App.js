70e89e0be00ae8413c4ca744e5b924b757f4afb5
prof-salu <saluorodrigues@gmail.com>
2026-09-14 09:40:55 -0300

    mod06 [finalizado]

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import {SafeAreaProvider, SafeAreaView} 
-                          from 'react-native-safe-area-context'; 
+import {SafeAreaProvider} from 'react-native-safe-area-context'; 
 
 //1. Importando as bibliotecas de navegação                          
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Ionicons} from '@expo/vector-icons';
+import { Ionicons} from '@expo/vector-icons'; //https://ionic.io/ionicons
 
 //2. Importando as telas
 import LoginScreen from './src/telas/LoginScreen';
@@ -18,6 +17,35 @@ import CatalogoScreen from './src/telas/CatalogoScreen';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+//5. Navegação por ABAS
+function TabNavegacao(){
+  return(
+    <Tab.Navigator screenOptions={({route}) => ({
+      tabBarActiveTintColor : '#27AE60', //Cor da aba selecionada
+      tabBarInactiveTintColor: '#CCC', //Cor da aba inativa
+      headerShown: false, //Esconde o cabeçalho
+      tabBarIcon : ({color, size}) => { //Colocando os icones das abas
+        let nomeIcone;
+
+        if(route.name === 'Loja'){
+          nomeIcone = 'basket';
+        }else if (route.name === 'Feed'){
+          nomeIcone = 'megaphone';
+        }else if(route.name === 'Perfil'){
+          nomeIcone = 'person';
+        }
+
+        return <Ionicons name={nomeIcone} color={color} size={size} />
+      }
+    })}>
+      
+      <Tab.Screen name='Loja' component={CatalogoScreen}/>
+      <Tab.Screen name='Feed' component={FeedScreen}/>
+      <Tab.Screen name='Perfil' component={PerfilScreen}/>
+    </Tab.Navigator>
+  )
+}
+
 //4. Configurando a pilha de navegação 
 export default function App() {
   return (
@@ -28,8 +56,8 @@ export default function App() {
           <Stack.Screen name='Login' component={LoginScreen} 
                         options={{headerShown: false}}/>
 
-          <Stack.Screen name='Catalogo' component={CatalogoScreen}/>
-          <Stack.Screen name='Feed' component={FeedScreen}/>
+          <Stack.Screen name='AreaLogada' component={TabNavegacao}
+                        options={{headerShown: false}}/>
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
--- src/telas/LoginScreen.js
+++ src/telas/LoginScreen.js
@@ -1,13 +1,26 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 
 export default function LoginScreen({navigation}){
     function fazerLogin(){
-        navigation.navigate('Catalogo');
+        navigation.navigate('AreaLogada');
     }
 
     return(
         <View style={styles.container}>
-            <Text style={styles.titulo}>Login Screen</Text>
+            <Text style={styles.titulo}>Hortifruti da Dona Gigi</Text>
+
+            <TextInput
+                style={styles.entrada}
+                placeholder='E-mail'
+                autoCapitalize='none'
+                keyboardType='email-address' />
+            
+            <TextInput 
+                style={styles.entrada}
+                placeholder='Senha'
+                autoCapitalize='none'
+                secureTextEntry={true} />
+
 
             <TouchableOpacity style={styles.botao} onPress={fazerLogin}>
                 <Text style={styles.textoBotao}>Entrar na loja</Text>
@@ -26,5 +39,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#27AE60', padding: 15, borderRadius: 8,
     }, textoBotao: {
         color: 'white', fontWeight: 'bold', fontSize: 16,
+    }, entrada: {
+        backgroundColor: 'white', borderWidth: 1, borderColor: '#BDC3C7', 
+        borderRadius: 8, padding: 15, marginBottom: 20, alignItems: 'center',
     }
 })
