21036c69f0e9dd53f6636b6ff4125f90651d54ed
prof-salu <saluorodrigues@gmail.com>
2026-09-21 08:49:36 -0300

    mod07[incompleto]

--- src/telas/LoginScreen.js
+++ src/telas/LoginScreen.js
@@ -1,8 +1,24 @@
-import {View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
+import {View, Text, StyleSheet, 
+        TouchableOpacity, TextInput, Alert} from 'react-native';
+import { useState } from 'react';
 
 export default function LoginScreen({navigation}){
+    //Estados
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
     function fazerLogin(){
-        navigation.navigate('AreaLogada');
+        
+        if(email.trim() === '' || senha.trim() === ''){
+            alert('Preencha sem e-mail e senha!');
+        }else{
+            if(email === 'teste@email.com' && senha === '1234'){
+                //Logado
+                navigation.navigate('AreaLogada');        
+            }else{
+                alert('Usuário ou senha incorretos.');
+            }
+        }        
     }
 
     return(
@@ -13,19 +29,26 @@ export default function LoginScreen({navigation}){
                 style={styles.entrada}
                 placeholder='E-mail'
                 autoCapitalize='none'
-                keyboardType='email-address' />
+                onChangeText={setEmail}
+                keyboardType='email-address'
+                value={email} />
             
             <TextInput 
                 style={styles.entrada}
                 placeholder='Senha'
                 autoCapitalize='none'
-                secureTextEntry={true} />
-
+                onChangeText={setSenha}
+                secureTextEntry={true} 
+                value={senha}/>
 
             <TouchableOpacity style={styles.botao} onPress={fazerLogin}>
                 <Text style={styles.textoBotao}>Entrar na loja</Text>
             </TouchableOpacity>
-        </View>
+
+            <TouchableOpacity style={styles.botaoLink}>
+                <Text style={styles.textoLink}>Anda não tem conta? Cadastre-se</Text>
+            </TouchableOpacity>
+        </View>        
     )
 }
 
@@ -34,7 +57,7 @@ const styles = StyleSheet.create({
         flex: 1, justifyContent: 'center', alignItems: 'center', 
         backgroundColor: '#F5F7FA',
     }, titulo: {
-        fontSize: 24, fontWeight: 'bold',
+        fontSize: 24, fontWeight: 'bold', marginBottom: 40,
     }, botao:{
         backgroundColor: '#27AE60', padding: 15, borderRadius: 8,
     }, textoBotao: {
@@ -42,5 +65,9 @@ const styles = StyleSheet.create({
     }, entrada: {
         backgroundColor: 'white', borderWidth: 1, borderColor: '#BDC3C7', 
         borderRadius: 8, padding: 15, marginBottom: 20, alignItems: 'center',
+    }, botaoLink: {
+        alignItems: 'center',
+    }, textoLink: {
+        color:'#2980b9', fontSize: 16, fontWeight: 'bold',
     }
 })
